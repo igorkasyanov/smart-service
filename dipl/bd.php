8edcfc6fc5b062606ec0c9159942fffa4eb42cec
igorkasyanov <105613902+igorkasyanov@users.noreply.github.com>
2022-06-06 21:53:36 +0600

    Add files via upload

---
+++ dipl/bd.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<nav>
+  <ul class="topmenu">
+    <li><a href="index.php">Smart</a></li>
+    <li><a href="tabl1.php">Ремонт смартфонов</a>
+    <li><a href="tabl2.php">Ремонт планшетов</a>
+    </li>
+    <li><a href="o_nas.php">О нас</a>
+    </li>
+    <li><a href="contact.php">Контакты</a></li>
+  </ul>
+</nav>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" type="text/css" href="css/tablstyle.css">
+	<title>Document</title>
+</head>
+<body>
+<?php
+  $host = '127.0.0.1';  
+  $user = 'root';    
+  $pass = 'root'; 
+  $db_name = 'smart';   
+  $link = mysqli_connect($host, $user, $pass, $db_name); 
+
+  if (!$link) {
+    echo 'Не могу соединиться с БД. Код ошибки: ' . mysqli_connect_errno() . ', ошибка: ' . mysqli_connect_error();
+    exit;
+  }
+ 
+  if (isset($_POST["service"])) {
+  
+    $sql = mysqli_query($link, "INSERT INTO `zakaz` (`service`, `model` , `email`) VALUES ('{$_POST['service']}', '{$_POST['model']}', '{$_POST['email']}')");
+  
+    if ($sql) {
+
+    } else {
+      echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
+    }
+  }
+  if (isset($_POST["model"])) {
+    
+    $sql = mysqli_query($link, "INSERT INTO `zakaz` (`service`, `model` , `email`) VALUES ('{$_POST['service']}', '{$_POST['model']}', '{$_POST['email']}')");
+    if ($sql) {
+
+     
+    } else 
+    {
+      echo '<p>Произошла ошибка: ' . mysqli_error($link) . '</p>';
+    }
+  }
+?>
+ <div id="container">
+  <h1>&bull; ремонт смартфонов &bull;</h1>
+  <div class="underline">
+  </div>
+  <form action="" method="post" id="contact_form">
+    <div class="email">
+      <label for="email"></label>
+      <input type="email" placeholder=" e-mail " name="email" id="email_input" required>
+    </div>
+    <div class="service">
+      <label for="name"></label>
+      <select placeholder="" name="subject" id="subject_input" required>
+        <option disabled hidden selected>Замена экрана</option>
+        <option>Замена батареи</option>
+        <option>Обновление ПО</option>
+        <option>Чистка динамиков (в зависимости от кол-ва)</option>
+        <option>Сбор/разбор</option>
+        <option>Замена датчика приближения</option>
+        <option>Замена разъема для наушников</option>
+        <option>замена фронтальной камеры</option>
+        <option>Замена основной камеры</option>
+        <option>Замена экрана</option>
+      </select>
+    </div>
+    <div class="model">
+      <label for="subject"></label>
+      <select placeholder="" name="subject" id="subject_input" required>
+        <option disabled hidden selected>xiaomi</option>
+        <option>asus</option>
+        <option>nokia</option>
+        <option>lenovo</option>
+        <option>hp</option>
+        <option>htc</option>
+        <option>samsung</option>
+        <option>xiaomi</option>
+        <option>meizu</option>
+      </select>
+    </div>
+    <div class="submit">
+      <input type="submit" value="Отправить" id="form_button" />
+    </div>
+  </form>
+</div>
+
+</html>
---
+++ dipl/js/text.js
@@ -0,0 +1,160 @@
+let table = document.createElement('table');
+let thead = document.createElement('thead');
+let tbody = document.createElement('tbody');
+
+
+table.appendChild(thead);
+table.appendChild(tbody);
+
+// Adding the entire table to the body tag
+document.getElementById('body').appendChild(table);
+
+// Creating and adding data to first row of the table
+let row_1 = document.createElement('tr');
+let heading_1 = document.createElement('th');
+heading_1.innerHTML = "Услуги ремонта смартфонов";
+let heading_2 = document.createElement('th');
+heading_2.innerHTML = "Время работы";
+let heading_3 = document.createElement('th');
+heading_3.innerHTML = "Стоимость";
+
+row_1.appendChild(heading_1);
+row_1.appendChild(heading_2);
+row_1.appendChild(heading_3);
+thead.appendChild(row_1);
+
+
+// Creating and adding data to second row of the table
+let row_2 = document.createElement('tr');
+let row_2_data_1 = document.createElement('td');
+row_2_data_1.innerHTML = "Замена экрана";
+let row_2_data_2 = document.createElement('td');
+row_2_data_2.innerHTML = "от 15 минут";
+let row_2_data_3 = document.createElement('td');
+row_2_data_3.innerHTML = "от 2500 руб";
+
+row_2.appendChild(row_2_data_1);
+row_2.appendChild(row_2_data_2);
+row_2.appendChild(row_2_data_3);
+tbody.appendChild(row_2);
+
+
+// Creating and adding data to third row of the table
+let row_3 = document.createElement('tr');
+let row_3_data_1 = document.createElement('td');
+row_3_data_1.innerHTML = "Замена батареи";
+let row_3_data_2 = document.createElement('td');
+row_3_data_2.innerHTML = "от 15 минут";
+let row_3_data_3 = document.createElement('td');
+row_3_data_3.innerHTML = "от 800 руб";
+
+row_3.appendChild(row_3_data_1);
+row_3.appendChild(row_3_data_2);
+row_3.appendChild(row_3_data_3);
+tbody.appendChild(row_3);
+
+// Creating and adding data to third row of the table
+let row_4 = document.createElement('tr');
+let row_4_data_1 = document.createElement('td');
+row_4_data_1.innerHTML = "Обновление ПО";
+let row_4_data_2 = document.createElement('td');
+row_4_data_2.innerHTML = "от 30 мин";
+let row_4_data_3 = document.createElement('td');
+row_4_data_3.innerHTML = "1090 руб";
+
+row_4.appendChild(row_4_data_1);
+row_4.appendChild(row_4_data_2);
+row_4.appendChild(row_4_data_3);
+tbody.appendChild(row_4);
+
+// Creating and adding data to third row of the table
+let row_5 = document.createElement('tr');
+let row_5_data_1 = document.createElement('td');
+row_5_data_1.innerHTML = "Чистка динамиков (в зависимости от кол-ва)";
+let row_5_data_2 = document.createElement('td');
+row_5_data_2.innerHTML = "от 40 мин";
+let row_5_data_3 = document.createElement('td');
+row_5_data_3.innerHTML = "1000-2500 руб";
+
+row_5.appendChild(row_5_data_1);
+row_5.appendChild(row_5_data_2);
+row_5.appendChild(row_5_data_3);
+tbody.appendChild(row_5);
+
+// Creating and adding data to third row of the table
+let row_6 = document.createElement('tr');
+let row_6_data_1 = document.createElement('td');
+row_6_data_1.innerHTML = "Сбор/разбор";
+let row_6_data_2 = document.createElement('td');
+row_6_data_2.innerHTML = "от 40 мин";
+let row_6_data_3 = document.createElement('td');
+row_6_data_3.innerHTML = "1490 руб";
+
+row_6.appendChild(row_6_data_1);
+row_6.appendChild(row_6_data_2);
+row_6.appendChild(row_6_data_3);
+tbody.appendChild(row_6);
+
+// Creating and adding data to third row of the table
+let row_7 = document.createElement('tr');
+let row_7_data_1 = document.createElement('td');
+row_7_data_1.innerHTML = "Замена датчика приближения ";
+let row_7_data_2 = document.createElement('td');
+row_7_data_2.innerHTML = "от 25 мин";
+let row_7_data_3 = document.createElement('td');
+row_7_data_3.innerHTML = "1000-1250 руб";
+
+row_7.appendChild(row_7_data_1);
+row_7.appendChild(row_7_data_2);
+row_7.appendChild(row_7_data_3);
+tbody.appendChild(row_7);
+
+// Creating and adding data to third row of the table
+let row_8 = document.createElement('tr');
+let row_8_data_1 = document.createElement('td');
+row_8_data_1.innerHTML = "Замена разъема для наушников";
+let row_8_data_2 = document.createElement('td');
+row_8_data_2.innerHTML = "от 20 мин";
+let row_8_data_3 = document.createElement('td');
+row_8_data_3.innerHTML = "800 руб";
+
+row_8.appendChild(row_8_data_1);
+row_8.appendChild(row_8_data_2);
+row_8.appendChild(row_8_data_3);
+tbody.appendChild(row_8);
+
+// Creating and adding data to third row of the table
+let row_9 = document.createElement('tr');
+let row_9_data_1 = document.createElement('td');
+row_9_data_1.innerHTML = "Замена фронтальной камеры ";
+let row_9_data_2 = document.createElement('td');
+row_9_data_2.innerHTML = "от 30 мин";
+let row_9_data_3 = document.createElement('td');
+row_9_data_3.innerHTML = "1500-5000 руб";
+
+row_9.appendChild(row_9_data_1);
+row_9.appendChild(row_9_data_2);
+row_9.appendChild(row_9_data_3);
+tbody.appendChild(row_9);
+
+// Creating and adding data to third row of the table
+let row_10 = document.createElement('tr');
+let row_10_data_1 = document.createElement('td');
+row_10_data_1.innerHTML = "Замена основной камеры ";
+let row_10_data_2 = document.createElement('td');
+row_10_data_2.innerHTML = "от 30 мин";
+let row_10_data_3 = document.createElement('td');
+row_10_data_3.innerHTML = "1500-5000 руб";
+
+row_10.appendChild(row_10_data_1);
+row_10.appendChild(row_10_data_2);
+row_10.appendChild(row_10_data_3);
+tbody.appendChild(row_10);
+
+
+
+
+
+
+
+
